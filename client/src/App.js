242602d043d795b89f67850a76c6c9ff8682e178
Swar028363 <chaudhariswar123@gmail.com>
2025-03-14 12:24:22 +0530

    removed routers from app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Routes from "./routes";
 
 function App() {
     return (
-        <Router>
-            <div className="flex flex-col justify-between">
-                <Header />
-                <div className="app-container">
-                    <div className="content">
-                        <Routes />
-                    </div>
+        <div className="flex flex-col justify-between">
+            <Header />
+            <div className="app-container">
+                <div className="content">
+                    <Routes />
                 </div>
-                <Footer />
             </div>
-        </Router>
+            <Footer />
+        </div>
     );
 }
 
